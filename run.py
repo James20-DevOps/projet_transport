0c5a1fcc326e307ab9dfa305c4085577fcc60c0f
JamesEd <edmondhomima@gmail.com>
2025-07-16 12:43:12 +0000

    essaie

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = create_app()
 #page d'accueil
 @app.route('/')
 def index():
-    return "Man debrouille toi ! "
+    return "<h1>Man debrouille toi !</h1>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # 🔥 important pour Render
