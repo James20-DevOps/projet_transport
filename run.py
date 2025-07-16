5c7e9cfbbc0a300fb0805301222a6c8c5c435d81
JamesEd <edmondhomima@gmail.com>
2025-07-16 12:34:42 +0000

    acceuil

--- run.py
+++ run.py
@@ -4,6 +4,11 @@ from app import create_app
 #initialisation de l'application
 app = create_app()
 
+#page d'accueil
+@app.route('/')
+def index():
+    return "Man debrouille toi ! "
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # 🔥 important pour Render
     app.run(host="0.0.0.0", port=port)
